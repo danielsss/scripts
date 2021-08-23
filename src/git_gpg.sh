d0575c6e049f40f5a552cbdcc4368b99ba5e4d81
danielsss <better.sunjian@gmail.com>
2021-08-23 14:20:10 +0800

    update: git_gpg.sh

--- src/git_gpg.sh
+++ src/git_gpg.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ];
-then
-  echo "Usage: ./git_gpg.sh --user='string value' --email='string value'"
+# Switch workspace
+cd $PWD;
+
+# Calculate input length
+if [ $# -lt 2 ]; then
+  echo "Usage: ./git_gpg.sh --user='string value' --email='string value'";
 fi
 
+# Assign input arguments
 EMAIL=
 NAME=
-PROJECT_F="${PWD}/.git/config"
-
 for item in "$@"; do
   case "${item}" in
     -u|--user=*)
-      NAME="${item#*=}"
+      NAME="${item#*=}";
     ;;
     -e|--email=*)
-      EMAIL="${item#*=}"
+      EMAIL="${item#*=}";
     ;;
     *)
       # Unknown Options
@@ -23,23 +25,26 @@ for item in "$@"; do
   esac
 done
 
-# check username
+# Check username
 if [ -z "$NAME" ]; then
-  echo "-u | --user must be non-empty"
-fi
-
-# check email
-if [ -z "$EMAIL" ]; then
-  echo "-e | --email must be non-empty"
-fi
-
-if [ ! -f "$PROJECT_F" ];
-then
-  echo "Not found .git/config"
+  echo "-u | --user must be non-empty";
   exit 1;
 fi
 
-# exit with error code if email is not valid
+# Check email
+if [ -z "$EMAIL" ]; then
+  echo "-e | --email must be non-empty";
+  exit 1;
+fi
+
+# Check project
+PROJECT_F="${PWD}/.git/config";
+if [ ! -f "$PROJECT_F" ]; then
+  echo "Not found .git/config";
+  exit 1;
+fi
+
+# Exit with error code if email is not valid
 gpg --list-secret-keys --keyid-format LONG ${EMAIL} > /dev/null 2>&1
 if [ ! $? -eq 0 ]; then
   echo "Error reading key: No secret key with you specified --email=${EMAIL}";
@@ -49,22 +54,16 @@ fi
 AUTO_SIGN=1
 SIGNING_KEY=$(gpg --list-secret-keys --keyid-format LONG ${EMAIL} | grep "sec" | awk '{print $2}' | awk -F/ '{print $2}')
 
-if [ ! -x $(which gpg) ];
-then
-  echo "Not found command 'gpg'"
+if [ ! -x $(which gpg) ]; then
+  echo "Not found command 'gpg'";
   exit 1;
 fi
 
-echo "Set user.name = ${NAME}"
+echo "git config --local user.name ${NAME}";
 git config --local user.name ${NAME}
-
-echo "Set user.email = ${EMAIL}"
+echo "git config --local user.email = ${EMAIL}";
 git config --local user.email ${EMAIL}
-
-echo "Set commit.gpgsign = 'true'"
+echo "git config --local commit.gpgsign = 'true'";
 git config --local commit.gpgsign "true"
-
-echo "Set user.signingkey = ${SIGNING_KEY}"
+echo "git config --local user.signingkey = ${SIGNING_KEY}";
 git config --local user.signingkey ${SIGNING_KEY}
-
-echo "done ..."
