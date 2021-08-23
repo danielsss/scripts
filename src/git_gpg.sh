4a7b4bc4d0e50b22993613d6dacf9062cb0a4769
danielsss <better.sunjian@gmail.com>
2021-08-23 14:13:13 +0800

    supported: input args

--- src/git_gpg.sh
+++ src/git_gpg.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/env bash
 
+if [ $# -lt 2 ];
+then
+  echo "Usage: ./git_gpg.sh --user='string value' --email='string value'"
+fi
+
+EMAIL=
+NAME=
 PROJECT_F="${PWD}/.git/config"
+
+for item in "$@"; do
+  case "${item}" in
+    -u|--user=*)
+      NAME="${item#*=}"
+    ;;
+    -e|--email=*)
+      EMAIL="${item#*=}"
+    ;;
+    *)
+      # Unknown Options
+    ;;
+  esac
+done
+
+# check username
+if [ -z "$NAME" ]; then
+  echo "-u | --user must be non-empty"
+fi
+
+# check email
+if [ -z "$EMAIL" ]; then
+  echo "-e | --email must be non-empty"
+fi
 
 if [ ! -f "$PROJECT_F" ];
 then
@@ -8,11 +39,15 @@ then
   exit 1;
 fi
 
-EMAIL="better.sunjian@gmail.com"
-NAME="danielsss"
+# exit with error code if email is not valid
+gpg --list-secret-keys --keyid-format LONG ${EMAIL} > /dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+  echo "Error reading key: No secret key with you specified --email=${EMAIL}";
+  exit 1;
+fi
+
 AUTO_SIGN=1
 SIGNING_KEY=$(gpg --list-secret-keys --keyid-format LONG ${EMAIL} | grep "sec" | awk '{print $2}' | awk -F/ '{print $2}')
-
 
 if [ ! -x $(which gpg) ];
 then
